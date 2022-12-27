a87600238e14d6bb8bc9f12341d8b8640ac42afc
Matheus Felipe Burigo <67557518+MatheusBurigo@users.noreply.github.com>
2022-12-27 19:51:35 -0300

    Primeira Classe

---
+++ Xadrez-Console/Xadrez-Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Xadrez_Console.Tabuleiro;
+
+namespace Xadrez_Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Position position;
+
+            position = new Position(3, 4);
+
+            Console.WriteLine("Posição " + position);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Xadrez-Console/Xadrez-Console/Tabuleiro/Position.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Xadrez_Console.Tabuleiro
+{
+    public class Position
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Position(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Coluna;
+        }
+    }
+}
