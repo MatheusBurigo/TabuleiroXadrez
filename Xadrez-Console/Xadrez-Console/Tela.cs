7bbd613010e0d5882fcab91b4682f93292476432
Matheus Felipe Burigo <67557518+MatheusBurigo@users.noreply.github.com>
2022-12-28 19:07:39 -0300

    Craindo diferença entre cor de peças e aplicando no Console

--- Xadrez-Console/Xadrez-Console/Tela.cs
+++ Xadrez-Console/Xadrez-Console/Tela.cs
@@ -1,4 +1,5 @@
 ﻿using Xadrez_Console.Tabuleiro;
+using Xadrez_Console.Tabuleiro.Enums;
 
 namespace Xadrez_Console
 {
@@ -9,7 +10,9 @@ namespace Xadrez_Console
             //Percorrer a Matriz de Linhas e colunas
             for (int i =0; i < tabuleiro.Linhas; i++)
             {
-                for (int j =0; j < tabuleiro.Colunas; j++)
+                Console.Write(8 - i + " ");
+
+                for (int j = 0; j < tabuleiro.Colunas; j++)
                 {
                     // Validação se não tiver nenhuma peça no tabuleiro
                     if (tabuleiro.peca(i,j) == null)
@@ -18,11 +21,29 @@ namespace Xadrez_Console
                     }
                     else
                     {
-                        Console.Write(tabuleiro.peca(i,j) + " ");
+                        ImprimirPeca(tabuleiro.peca(i, j));
+                        Console.Write(" ");
                     }
                 }
                 // Representa o final da linha e quebra para uma nova coluna
                 Console.WriteLine();
+            }
+            Console.WriteLine(" a b c d e f g h");
+        }
+
+        public static void ImprimirPeca(Peca peca)
+        {
+            if (peca.Cor == Cor.Branco)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                //Alterando cor da Peça no Console
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
