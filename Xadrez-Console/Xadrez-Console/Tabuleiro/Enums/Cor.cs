a16cee4861b27d8be20782c287c6942f320a7618
Matheus Felipe Burigo <67557518+MatheusBurigo@users.noreply.github.com>
2022-12-28 14:24:31 -0300

    Implementando Tabuleiro

---
+++ Xadrez-Console/Xadrez-Console/Tabuleiro/Enums/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez_Console.Tabuleiro.Enums
+{
+    public enum Cor
+    {
+        Vermelho,
+        Laranja,
+        Amarelo,
+        Verde,
+        Azul,
+        Anil,
+        Violeta
+    }
+}
---
+++ Xadrez-Console/Xadrez-Console/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using Xadrez_Console.Tabuleiro.Enums;
+
+namespace Xadrez_Console.Tabuleiro
+{
+    public class Peca
+    {
+        public Position Position { get; set; }
+        public Cor Cor{ get; protected set; }
+        public int MovimentosRealizados { get; protected set; }
+        public Tabuleiro Tabuleiro{ get; protected set; }
+
+        public Peca(Position position, Cor cor, Tabuleiro tabuleiro)
+        {
+            Position = position;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            MovimentosRealizados= 0;
+        }
+    }
+}
